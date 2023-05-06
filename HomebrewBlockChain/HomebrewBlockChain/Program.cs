5c31bbdbacb2d9bbbea031e1ed0b997f45da48a5
slinak <scott@linak.org>
2023-05-06 06:45:26 -0700

    created input loop

--- HomebrewBlockChain/HomebrewBlockChain/Program.cs
+++ HomebrewBlockChain/HomebrewBlockChain/Program.cs
@@ -8,6 +8,7 @@ namespace HomebrewBlockChain
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Initializing blockchain and creating genesis block");
@@ -16,8 +17,41 @@ namespace HomebrewBlockChain
             Console.WriteLine("---------------------------");
             blockChain.MineBlock();
             Console.WriteLine(blockChain.GetFullChain());
+
+            ProcessChainInputAction(blockChain);
             Console.ReadLine();
 
+        }
+
+        static void ProcessChainInputAction(BlockChainModel.BlockChain blockChain)
+        {
+            string command = "C";
+            bool stop = false;
+            while (!stop)
+            {
+                Console.WriteLine("Enter command:");
+                Console.WriteLine("(M)ine | e(X)it | (P)rint Chain | (R)esolve Conflicts");
+                command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "m":
+                    case "M":
+                        Console.WriteLine("Mining block...");
+                        Console.WriteLine(blockChain.MineBlock());
+                        break;
+                    case "p":
+                    case "P":
+                        Console.WriteLine("Printing full chain...");
+                        Console.WriteLine(blockChain.GetFullChain());
+                        break;
+                    case "x":
+                    case "X":
+                        stop = true;
+                        Console.WriteLine("Stopping input loop...");
+                        break;
+                }
+            }
         }
     }
 }
